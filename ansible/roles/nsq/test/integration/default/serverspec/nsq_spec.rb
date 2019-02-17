b722bc8cf8458b27f1eed280e72ac05d84a95e37
ZeroC0D3 Engineer <dfdenni0912@gmail.com>
2019-02-17 18:44:49 +0700

    Restructure playbooks

--- ansible/roles/nsq/test/integration/default/serverspec/nsq_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'NSQ role general specs' do
-  %w(nsq__pubsub nsq__stat nsq__tail nsq__to_file nsq__to_http nsq__to_nsq to_nsq).each do |binary|
-    describe file("/usr/local/bin/#{binary}") do
-      it { should exist }
-    end
-  end
-end
