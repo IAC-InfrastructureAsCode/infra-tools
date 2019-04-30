705a9dbf8864c38c6f34dd11ecf194a23e2fe535
dfdenni <dfdenni0912@gmail.com>
2019-04-30 09:45:01 +0700

    Change license from MIT to Apache v2

--- dockerframework/docker-cmd.sh
+++ dockerframework/docker-cmd.sh
@@ -4,7 +4,7 @@
 # -----------------------------------------------------------------------------
 #  Author     : Dwi Fahni Denni (@zeroc0d3)
 #  Repository : https://github.com/zeroc0d3/docker-framework
-#  License    : MIT
+#  License    : Apache v2
 # -----------------------------------------------------------------------------
 
 case $1 in
