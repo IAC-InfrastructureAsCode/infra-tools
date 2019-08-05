7971089fddd0770bb1b63b260171189ba55e80fe
DFDenni <dwi.fahni@bukalapak.com>
2019-08-05 22:54:32 +0700

    Update terraform version to 0.12.6

--- script/installer/install_terraform.sh
+++ script/installer/install_terraform.sh
@@ -5,7 +5,7 @@
 # ================================================================================================
 export DEBIAN_FRONTEND=noninteractive
 
-export TERRAFORM_VERSION="0.11.11"
+export TERRAFORM_VERSION="0.12.6"
 
 if ! [ "${TF_VERSION}" = "" ]
 then
